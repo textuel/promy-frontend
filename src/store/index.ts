1444136c6301611ac8635060e94bc23103f8452f
alstn2468 <alstn2468_@naver.com>
2021-03-07 16:08:53 +0900

    redux-toolkit으로 logger, devtools 설정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 const store = configureStore({
     reducer: {},
+    middleware: (getDefaultMiddleware) => {
+        if (process.env.NODE_ENV === 'production') {
+            return getDefaultMiddleware();
+        }
+        return [...getDefaultMiddleware(), logger];
+    },
+    devTools: process.env.NODE_ENV === 'development',
 });
 
 export default store;
