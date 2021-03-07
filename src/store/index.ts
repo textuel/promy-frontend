60df3bf3e79d134d8a63eefed8891b6ab9cb7c39
Minsu Kim <alstn2468_@naver.com>
2021-03-07 16:27:27 +0900

    redux, redux-toolkit 설정 (#3)
    * redux, redux-toolkit 설정, resolver 경로 변경
    
    * redux-toolkit으로 logger, devtools 설정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,15 @@
-const store = null;
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+const store = configureStore({
+    reducer: {},
+    middleware: (getDefaultMiddleware) => {
+        if (process.env.NODE_ENV === 'production') {
+            return getDefaultMiddleware();
+        }
+        return [...getDefaultMiddleware(), logger];
+    },
+    devTools: process.env.NODE_ENV === 'development',
+});
 
 export default store;
