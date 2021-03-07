a8b2114457cfe6201853aeb07e4e76e5eb77f74c
Minsu Kim <alstn2468_@naver.com>
2021-03-07 15:40:12 +0900

    emotion 설정 추가 (#2)

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,20 @@
-const { alias, configPaths } = require('react-app-rewire-alias');
+const {
+    override,
+    addBundleVisualizer,
+    addWebpackAlias,
+    addBabelPlugins,
+} = require('customize-cra');
+const path = require('path');
 
-module.exports = function override(config) {
-    return alias(configPaths('./tsconfig.paths.json'))(config);
-};
+module.exports = override(
+    process.env.NODE_ENV === 'development' && addBundleVisualizer(),
+    ...addBabelPlugins('@emotion/babel-plugin'),
+    addWebpackAlias({
+        '@components/*': path.resolve(__dirname, './src/components/*'),
+        '@reducers/*': path.resolve(__dirname, './src/reducers/*'),
+        '@store/*': path.resolve(__dirname, './src/store/*'),
+        '@actions/*': path.resolve(__dirname, './src/actions/*'),
+        '@types/*': path.resolve(__dirname, './src/types/*'),
+        '@utils/*': path.resolve(__dirname, './src/utils/*'),
+    }),
+);
